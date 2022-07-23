1b78533b0b09a82a49695880a2b251666ea144e3
Alma Perez <aperez32@student.scf.edu>
2022-07-23 15:41:46 -0400

    Array Lab Done

--- index.js
+++ index.js
@@ -32,3 +32,31 @@
 //     return newCats;
 // }
 
+const cats = ["Milo", "Otis", "Garfield"];
+function destructivelyAppendCat(name){
+cats.push(name);}
+function destructivelyPrependCat(name){
+    cats.unshift(name);
+}
+function destructivelyRemoveLastCat (){
+    cats.pop();
+}
+function destructivelyRemoveFirstCat(){
+    cats.shift();
+}
+function appendCat(name){
+    const newCats = cats.concat(name);
+    return newCats;
+}
+function prependCat(name){
+    const newCats = [name ,"Milo", "Otis","Garfield" ]
+    return newCats;
+}
+function removeLastCat() {
+const newCats =["Milo", "Otis" ]
+return newCats;
+}
+function removeFirstCat() {
+    const newCats =[ "Otis","Garfield" ]
+    return newCats;
+}
